feb8a532f87ab7b89bd6095ca2bfbbc86b2a1220
Kimiblock <61745317+Kimiblock@users.noreply.github.com>
2022-05-28 13:18:12 +0800

    Update Update.sh

--- Update.sh
+++ Update.sh
@@ -1,8 +1,45 @@
-export version=1.18.2
-export serverPath=/mnt/main/Cache/Paper
-echo "Hello! `whoami`"
+#!/bin/bash
+######User Settings Start######
+#export version=1.18.2
+#export serverPath=/mnt/main/Cache/Paper
+######User Settings End######
+
+echo "Hello! `whoami` at `date`"
+echo "Reading settings"
 
 ######Function Start######
+#testPackageManager
+function detectPackageManager(){
+    if [[ $(sudo apt install 2>/dev/null) ]]; then
+        echo 'Detected apt'
+        return apt
+    elif [[ $(sudo pacman -h 2>/dev/null) ]]; then
+        echo 'Detected pacman'
+        return pacman
+    elif [[ $(sudo dnf install 2>/dev/null) ]]; then
+        echo 'Detected dnf'
+        return dnf
+    else
+        return unknown
+    fi
+}
+#checkConfig
+checkConfig(){
+    if [ ! ${version} ]; then
+        echo '$version not set, default to 1.18.2...'
+        export version=1.18.2
+    fi
+    if [ ! ${serverPath} ]; then
+        echo "Warning! serverPath not set, exiting..."
+        exit 1
+    fi
+}
+#removeJarFile
+function clean(){
+    if [ -f *.jar ]; then
+        rm -rf *.jar
+    fi
+}
 #moveFile
 function update(){
     if [ $@ = Paper-latest.jar ]; then
@@ -11,24 +48,46 @@ function update(){
         mv $@ ${serverPath}/plugins/
     fi
 }
-
 #versionCompare
 function versionCompare(){
-    if [ ${isPlugin} = true ]; then
+    if [ $isPlugin = true ]; then
         checkPath="${serverPath}/plugins"
     else
         checkPath="${serverPath}"
     fi
-
-    if [ "(md5sum ${checkPath}/${checkFile})" = "(md5sum ${checkFile})" ]; then
-        export same=1
-        echo "${checkFile} checked, you're up to date."
+    diff -q '${checkPath}/${checkFile}' '${checkFile}'
+    return $?
+}
+#integrityProtect
+function integrityProtect(){
+    if [ ${isPlugin} = false ]; then
+        checkFile=Paper-latest.jar
+        wget $url
+        mv paper-*.*.*.jar Paper-latest.jar.check
+        diff -q Paper-latest.jar.check Paper-latest.jar
+        return $?
     else
-        export same=0
-        echo "${checkFile} checked, updates are available."
+        mv $checkFile ${checkFile.check}
+        wget $url
+        diff -q $checkFile ${checkFile.check}
+    fi
+    if [ $? = 1 ]; then
+        redownload
     fi
 }
-
+function redownload(){
+    if [ ${isPlugin} = false ]; then
+        clean
+        checkFile=Paper-latest.jar
+        wget $url
+        mv paper-*.*.*.jar Paper-latest.jar
+        integrityProtect
+    else
+        clean
+        wget $url
+        integrityProtect
+    fi
+}
 #pluginUpdate
 function pluginUpdate(){
     if [ $@ = Floodgate ]; then
@@ -49,6 +108,7 @@ function pluginUpdate(){
 ######Function End######
 
 ######Paper Update Start######
+checkConfig
 echo "Starting auto update for PaperMC, SAC and GeyserMC at `date`"
 export build=700
 cd ${serverPath}/Update/
@@ -56,41 +116,60 @@ echo "Starting download at `date`"
 while [ ! -f paper-*.jar ]; do
     export build=`expr ${build} - 1`
     echo "Testing build ${build}"
-    wget https://papermc.io/api/v2/projects/paper/versions/${version}/builds/${build}/downloads/paper-${version}-${build}.jar
-    if [ -f paper-*.jar ]; do
-        mv paper-*.jar Paper-latest.jar
-    fi
+    url="https://papermc.io/api/v2/projects/paper/versions/${version}/builds/${build}/downloads/paper-${version}-${build}.jar"
+    wget $url
 done
+if [ -f paper-*.jar ]; then
+    mv paper-*.jar Paper-latest.jar
+fi
 export isPlugin=false
 export checkFile=Paper-latest.jar
+integrityProtect
 versionCompare
-if [ $same = 1 ]; then
+if [ $? = 0 ]; then
     rm Paper-latest.jar
 else
     update Paper-latest.jar
 fi
+clean
 ######Paper Update End######
 
 ######Plugin Update Start######
+export isPlugin=true
 pluginUpdate Geyser
+export checkFile='Geyser-Spigot.jar'
+integrityProtect
 versionCompare
 update *.jar
+clean
 
+export isPlugin=true
+export checkFile='floodgate-spigot.jar'
 pluginUpdate Floodgate
 versionCompare
 update *.jar
+clean
 ######Plugin Update End######
 
 ######System Update Start######
 echo "Notice: Script will try to do a full system update"
 if [[ `whoami` = root ]]; then
-    if [[ $(sudo apt install 2>/dev/null) ]]; then
-        echo 'Detected apt' && sudo apt -y full-upgrade
-    elif [[ $(sudo pacman -h 2>/dev/null) ]]; then
-        echo 'Detected pacman' && sudo pacman --noconfirm -Syu
-    elif [[ $(sudo dnf install 2>/dev/null) ]]; then
-        echo 'Detected dnf' && sudo dnf -y dnf update
-    fi
+    detectPackageManager
+    if [ $? = "apt" ]; then
+        apt -y full-upgrade
+    elif [ $? = "dnf" ]; then
+        dnf -y dnf update
+    elif [ $? = "pacman" ]; then
+        pacman --noconfirm -Syyu
+    else
+        unset packageManager
+        echo "Package Manager not found! Enter command to update or type 'skip' to skip"
+        read packageManager
+        if [ ! ${packageManager} = "skip" ]; then
+            $packageManager
+        else
+            echo "Skipping"
+        fi
 else
     echo "Update Failed! You are running under `whoami`"
 fi
@@ -101,5 +180,7 @@ unset version
 unset serverPath
 unset checkPath
 unset isPlugin
-rm -r *.jar
+unset packageManager
+unset checkFile
+clean
 ######Clean Environment Variables End######
